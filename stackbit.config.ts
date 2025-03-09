3cb197bcff45e71aefc90fa685affbaefa0bd6f5
JoshChuaYiJie <JoshChuaYiJie@gmail.com>
2025-03-09 12:53:39 +0800

    Create stackbit.config.ts

---
+++ stackbit.config.ts
@@ -0,0 +1,32 @@
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["_posts"], // Where your posts are stored
+      models: [
+        {
+          name: "Post",
+          type: "page",
+          urlPath: "/blog/{slug}",
+          filePath: "_posts/{year}-{month}-{day}-{slug}.md",
+          fields: [
+            { name: "date", type: "datetime", required: true },
+            { name: "layout", type: "string", required: true, default: "post" },
+            { name: "title", type: "string", required: true },
+            { name: "subtitle", type: "string", required: false },
+            { name: "description", type: "string", required: true },
+            { name: "image", type: "image", required: true },
+            { name: "optimized_image", type: "image", required: false },
+            { name: "category", type: "string", required: true },
+            { name: "tags", type: "list", items: { type: "string" }, required: false },
+            { name: "author", type: "string", required: false },
+            { name: "paginate", type: "boolean", required: false }
+          ]
+        }
+      ],
+    })
+  ]
+});
